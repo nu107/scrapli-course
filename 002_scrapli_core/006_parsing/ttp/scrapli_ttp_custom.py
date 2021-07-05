0a6b99c44ee5eeab068cbe14c5ba7c47326a0470
Rick Donato <rickj07@gmail.com>
2021-07-05 21:17:39 +0100

    Remove helper function

--- 002_scrapli_core/006_parsing/ttp/scrapli_ttp_custom.py
+++ 002_scrapli_core/006_parsing/ttp/scrapli_ttp_custom.py
@@ -15,19 +15,15 @@ device = {
     "platform": "arista_eos",
 }
 
-# Helper function 
-def get_absolute_path(filename):
-    # Get absolute path to command file
-    absolute_path = os.path.dirname(os.path.realpath(__file__))
-    return f"{absolute_path}/{filename}"
-
 # Scrapli context manager - Instantiate, open and close connection
 with Scrapli(**device) as conn:
     # Send command to device
     response = conn.send_command("show interfaces")
 
     # Get TTP parsed response
-    ttp_template = get_absolute_path("eos_show_interfaces.ttp")
+    ttp_template = (
+        f"{os.path.dirname(os.path.realpath(__file__))}/eos_show_interfaces.ttp"
+    )
     ttp_parsed_response = response.ttp_parse_output(template=ttp_template)
 
 # Print parsed response
